a37bf364cbc0a9c02ca4353361166935a1791fd6
Nikita <nikitamir558@gmail.com>
2024-04-16 17:30:46 +0300

    my-project

--- script.js
+++ script.js
@@ -1,67 +1,105 @@
-let numberOfFilms
-
-function start() {
-  numberOfFilms = +prompt('Сколько посмотрели?', '')
-
-  while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('Сколько посмотрели?', '')
-  }
-}
-
-start()
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
-  private: false
-}
-
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних ?', '').trim()
-    const b = prompt('Какая оценка?', '')
+  private: false,
+  start: function() {
+    personalMovieDB.count = +prompt('Сколько посмотрели?', '')
   
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-      personalMovieDB.movies[a] = b
-      console.log('done')
+    while (personalMovieDB === '' || personalMovieDB === null || isNaN(personalMovieDB.count)) {
+      personalMovieDB.count = +prompt('Сколько посмотрели?', '')
+    }
+  },
+  rememberMyFilms: function() {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('Один из последних ?', '')
+      const b = prompt('Какая оценка?', '')
+    
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b
+        console.log('done')
+      } else {
+        console.log('error')
+        i--
+      }
+    }
+  },
+  detectPersonalLevel: function() {
+    if(personalMovieDB.count < 10) {
+      console.log('Просмотрено мало')
+    } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log('Классический зритель')
+    } else if(personalMovieDB.count >= 30) {
+      console.log('Киноман');
     } else {
-      console.log('error')
-      i--
+      console.log('Ошибка');
+    }
+  },
+  showMyDb: function(hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },  
+  writeYourGenres: function() {
+    for(let i = 1; i <= 3; i++) {
+      let genre = prompt(`Любимый жанр под номером ${i}`)
+
+      if(genre === '' || genre === null) {
+        console.log('Error!')
+        i--
+
+      } else {
+        personalMovieDB.genres[i - 1] = genre
+      }
+
+      personalMovieDB.genres.forEach((item, i) => {
+        console.log(`Любимый жанр ${i + 1} -это ${item}`) //item - Каждый элемент в массиве, который перебираем. i - это номер по порядку
+      })
+      /* personalMovieDB.genres.push() */
+      /* let genre = prompt(`Любимый жанр под номером ${i}`)
+      personalMovieDB.genres.push(genre) */
+    }
+  },
+  toggleVisibleMyDB: function() {
+    if (personalMovieDB.private) {
+      personalMovieDB.private = false
+    } else {
+      personalMovieDB.private = true
     }
   }
+
 }
 
-rememberMyFilms()
 
-function detectPersonalLevel() {
-  if(personalMovieDB.count < 10) {
-    console.log('Просмотрено мало')
-  } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Классический зритель')
-  } else if(personalMovieDB.count >= 30) {
-    console.log('Киноман');
-  } else {
-    console.log('Ошибка');
+/* const arr = [1, 2, 3, 6, 8]
+
+arr.forEach((value, i, arr) => {
+  console.log(`${i}: ${value}, внутри массива ${arr}`);
+}, 1000)
+
+/* arr.pop()
+arr.push(10)
+console.log(arr); */
+
+/* const str = prompt('', '')
+const products = str.split(', ')
+products.sort()
+console.log(products.join('; '));  */
+/* const soldier = {
+  hp: 400,
+  armor: 100,
+  sayHello: function() {
+    console.log('hello');
   }
 }
 
-detectPersonalLevel()
+const john = Object.create(soldier)
 
-function showMyDb (hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
+/* const soldJon = {
+  hp: 100,
+  
+} */
 
-showMyDb(personalMovieDB.private)
-
-function writeYourGenres() {
-  for(let i = 1; i <= 3; i++) {
-    let genre = prompt(`Любимый жанр под номером ${i}`)
-    personalMovieDB.genres.push(genre)
-  }
-}
-
-writeYourGenres()
+/* Object.setPrototypeOf(john, soldier)
+john.sayHello() */ 
