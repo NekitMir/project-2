65fa8e42017284007f933550736475b7c4eeabc3
Nikita <nikitamir558@gmail.com>
2024-04-16 23:21:29 +0300

    refactor-project

--- script.js
+++ script.js
@@ -1,105 +1,26 @@
-const personalMovieDB = {
-  count: 0,
-  movies: {},
-  actors: {},
-  genres: [],
-  private: false,
-  start: function() {
-    personalMovieDB.count = +prompt('Сколько посмотрели?', '')
-  
-    while (personalMovieDB === '' || personalMovieDB === null || isNaN(personalMovieDB.count)) {
-      personalMovieDB.count = +prompt('Сколько посмотрели?', '')
-    }
-  },
-  rememberMyFilms: function() {
-    for (let i = 0; i < 2; i++) {
-      const a = prompt('Один из последних ?', '')
-      const b = prompt('Какая оценка?', '')
-    
-      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b
-        console.log('done')
-      } else {
-        console.log('error')
-        i--
-      }
-    }
-  },
-  detectPersonalLevel: function() {
-    if(personalMovieDB.count < 10) {
-      console.log('Просмотрено мало')
-    } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-      console.log('Классический зритель')
-    } else if(personalMovieDB.count >= 30) {
-      console.log('Киноман');
-    } else {
-      console.log('Ошибка');
-    }
-  },
-  showMyDb: function(hidden) {
-    if (!hidden) {
-      console.log(personalMovieDB);
-    }
-  },  
-  writeYourGenres: function() {
-    for(let i = 1; i <= 3; i++) {
-      let genre = prompt(`Любимый жанр под номером ${i}`)
+const box = document.getElementById('box'),
+      btn = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper')
 
-      if(genre === '' || genre === null) {
-        console.log('Error!')
-        i--
+box.style.backgroundColor = 'blue'
 
-      } else {
-        personalMovieDB.genres[i - 1] = genre
-      }
-
-      personalMovieDB.genres.forEach((item, i) => {
-        console.log(`Любимый жанр ${i + 1} -это ${item}`) //item - Каждый элемент в массиве, который перебираем. i - это номер по порядку
-      })
-      /* personalMovieDB.genres.push() */
-      /* let genre = prompt(`Любимый жанр под номером ${i}`)
-      personalMovieDB.genres.push(genre) */
-    }
-  },
-  toggleVisibleMyDB: function() {
-    if (personalMovieDB.private) {
-      personalMovieDB.private = false
-    } else {
-      personalMovieDB.private = true
-    }
-  }
-
-}
-
-
-/* const arr = [1, 2, 3, 6, 8]
-
-arr.forEach((value, i, arr) => {
-  console.log(`${i}: ${value}, внутри массива ${arr}`);
-}, 1000)
-
-/* arr.pop()
-arr.push(10)
-console.log(arr); */
-
-/* const str = prompt('', '')
-const products = str.split(', ')
-products.sort()
-console.log(products.join('; '));  */
-/* const soldier = {
-  hp: 400,
-  armor: 100,
-  sayHello: function() {
-    console.log('hello');
-  }
-}
-
-const john = Object.create(soldier)
-
-/* const soldJon = {
-  hp: 100,
-  
+/* for(let i = 0; i < hearts.length; i++) {
+  hearts[i].style.backgroundColor = 'black'
 } */
+hearts.forEach(item => {
+  item.style.backgroundColor = 'gold'
+})
 
-/* Object.setPrototypeOf(john, soldier)
-john.sayHello() */ 
+const div = document.createElement('div')
+//const text = document.createTextNode('Hello!')
+div.classList.add('black')
+
+//document.body.append(div)
+//wrapper.append(div)
+wrapper.prepend(div)
+      
+
+
